4aacd00aa47b9f107ee72c67da5b62747a32f366
dcbarros1 <dcbarros1@gmail.com>
2021-11-09 10:59:05 -0300

    Alteração dos arquivos de header-clinica e header-patient e a finalização da aba de dashboard

---
+++ assets/js/dashboardPlots.js
@@ -0,0 +1,29 @@
+const config = {
+    type: 'line',
+    data: data,
+    options: {}
+};
+
+const labels = [
+    'January',
+    'February',
+    'March',
+    'April',
+    'May',
+    'June',
+];
+
+const data = {
+    labels: labels,
+    datasets: [{
+        label: 'My First dataset',
+        backgroundColor: 'rgb(255, 99, 132)',
+        borderColor: 'rgb(255, 99, 132)',
+        data: [0, 10, 5, 2, 20, 30, 45],
+    }]
+};
+
+const myChart = new Chart(
+    document.getElementById('myChart'),
+    config
+  );
